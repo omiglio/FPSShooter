da96074d65b4bc166bb21a1fc19cef59230fb392
omiglio <ogalvan5@student.mtsac.edu>
2022-03-11 08:44:54 -0800

    1. Building The Game, Adding LFS, & Creating The Settings in UE4.

---
+++ FPSShooter/Source/FPSShooter/FPSShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPSShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFPSShooterGameMode();
+};
+
+
+
